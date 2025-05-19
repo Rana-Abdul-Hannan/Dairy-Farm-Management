08f4fd73db6e8f005256b42eb8ad9aa9e9dc2ac4
Rana-Abdul-Hannan <bsai23065@itu.edu.pk>
2025-05-19 16:51:18 +0500

    Delete DairyFarm_Sprint1 directory

--- DairyFarm_Sprint1/backend/models/Animal.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const AnimalSchema = new mongoose.Schema({
-  name: String,
-  breed: String,
-  age: Number
-});
-
-module.exports = mongoose.model('Animal', AnimalSchema);
